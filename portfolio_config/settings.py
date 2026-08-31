45d3b637178b8c2580fa861f7c09aaca30e4ec5c
Shaurya170319 <Shaurya22256@gmail.com>
2026-09-01 00:08:55 +0530

    Updated

--- portfolio_config/settings.py
+++ portfolio_config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME', 'localhost'), '127.0.0.1']
 
 
 # Application definition
