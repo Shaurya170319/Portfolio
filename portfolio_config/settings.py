a7fecc164e09f460d218da79de9ca36fa474deb6
Shaurya170319 <Shaurya22256@gmail.com>
2026-09-01 20:22:22 +0530

    Fix DEBUG default to True locally

--- portfolio_config/settings.py
+++ portfolio_config/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME', 'localhost'), '127.0.0.1']
 
